cc06b823275cbe0520e123a92e490915de3a72c4
Sven Olding <sven.olding@gmx.de>
2023-11-05 12:20:12 +0100

    first commit

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/respository/RestaurantRepository.java
@@ -0,0 +1,10 @@
+package com.food.ordering.system.order.service.domain.ports.output.respository;
+
+import com.food.ordering.system.order.service.domain.entity.Restaurant;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+public interface RestaurantRepository {
+    Optional<Restaurant> findRestaurantInformation(Restaurant restaurant);
+}
