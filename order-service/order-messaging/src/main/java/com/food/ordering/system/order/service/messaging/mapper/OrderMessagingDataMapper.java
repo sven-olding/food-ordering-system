e49f3b6a00414dbce570b96c9f96ade82d6a2d2c
Sven Olding <solding@gedys-intraware.de>
2023-11-19 13:52:08 +0100

    add kafka publisher implementations

--- order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/mapper/OrderMessagingDataMapper.java
+++ order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/mapper/OrderMessagingDataMapper.java
@@ -1,10 +1,10 @@
 package com.food.ordering.system.order.service.messaging.mapper;
 
-import com.food.ordering.system.kafka.order.avro.model.PaymentOrderStatus;
-import com.food.ordering.system.kafka.order.avro.model.PaymentRequestAvroModel;
+import com.food.ordering.system.kafka.order.avro.model.*;
 import com.food.ordering.system.order.service.domain.entity.Order;
 import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
 import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
+import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -34,6 +34,22 @@ public class OrderMessagingDataMapper {
                 .setCreatedAt(orderCancelledEvent.getCreatedAt().toInstant())
                 .setPrice(order.getPrice().getAmount())
                 .setPaymentOrderStatus(PaymentOrderStatus.CANCELLED)
+                .build();
+    }
+
+    public RestaurantApprovalRequestAvroModel orderPaidEventToRestaurantApprovalRequestAvroModel(OrderPaidEvent orderPaidEvent) {
+        Order order = orderPaidEvent.getOrder();
+        return RestaurantApprovalRequestAvroModel.newBuilder()
+                .setOrderId(order.getId().getValue().toString())
+                .setRestaurantId(order.getRestaurantId().getValue().toString())
+                .setId(UUID.randomUUID().toString())
+                .setSagaId("")
+                .setCreatedAt(orderPaidEvent.getCreatedAt().toInstant())
+                .setRestaurantOrderStatus(RestaurantOrderStatus.PAID)
+                .setProducts(order.getItems().stream().map(orderItem -> Product.newBuilder()
+                        .setId(orderItem.getProduct().getId().getValue().toString())
+                        .setQuantity(orderItem.getQuantity())
+                        .build()).toList())
                 .build();
     }
 }
