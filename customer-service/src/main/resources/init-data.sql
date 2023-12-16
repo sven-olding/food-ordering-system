edfab9128b3a7a05ae31103dabb0740a431b7d82
Sven Olding <solding@gedys-intraware.de>
2023-12-16 13:45:18 +0100

    add customer service

---
+++ customer-service/src/main/resources/init-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer.customers(id, username, first_name, last_name)
+VALUES ('51f38358-f952-43a7-9822-e979c1e8ebe9', 'user_1', 'First', 'User');
+
+INSERT INTO customer.customers(id, username, first_name, last_name)
+VALUES ('15f2f7b9-eb37-403b-8a04-c5a6a5027c34', 'user_2', 'Second', 'User');
